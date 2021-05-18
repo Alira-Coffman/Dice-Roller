426551c3581c3c206a32e62bc590ec05577e75be
Alira Coffman <40637263+Alira-Coffman@users.noreply.github.com>
2021-05-18 12:55:17 -0500

    Add files via upload
    added dice front end trigger & modifier inputs

--- script.js
+++ script.js
@@ -18,3 +18,75 @@ function rollDice(faceAmount, amountRolled, modifier)
     console.log(diceRolledArray);
     return diceOutcome + modifier;
 }
+
+
+function roll() {
+    //get dice face value
+    const rbs = document.querySelectorAll('input[name="options"]');
+            let selectedValue;
+            for (const rb of rbs) {
+                if (rb.checked) {
+                    selectedValue = rb.value;
+                    break;
+                }
+            }
+            
+    var faceNumber = selectedValue;
+
+    console.log(faceNumber);
+    //get number of dice
+    var numberOfDice = document.getElementById("diceNumberInput").value;
+
+    //get number of modifier
+    var modifierValue =  document.getElementById("modifierInput").value;
+
+
+    //rollDice
+    rollDice(faceNumber, numberOfDice, modifierValue);
+    
+}
+
+function modifier(upDown) {
+    var x = document.getElementById("modifierInput");
+    var currentVal = x.value;
+    var newValue;
+
+    if(upDown)
+    {
+        //increment
+       
+         newValue = 1+ Number(currentVal)
+      
+    }
+    else
+    {
+        //decrement
+        
+        newValue = Number(currentVal) - 1;
+       
+
+    }
+    document.getElementById("modifierInput").value = newValue;
+    
+}
+
+function diceNumberSet(upDown) {
+    var x = document.getElementById("diceNumberInput");
+    var currentVal = x.value;
+    var newValue;
+
+    if(upDown)
+    {
+        //increment
+       
+         newValue = 1+ Number(currentVal)
+
+    }
+    else
+    {
+        //decrement
+        newValue = Number(currentVal) - 1;
+    }
+    document.getElementById("diceNumberInput").value = newValue;
+    
+}
